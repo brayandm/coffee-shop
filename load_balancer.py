7db2a0dabf46027b6458f7f7e01ca014a3658a79
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-17 20:30:25 +0200

    load balancer fixed

--- load_balancer.py
+++ load_balancer.py
@@ -1,11 +1,45 @@
 from flask import Flask, jsonify, request, redirect
-import random
+import requests
 
 app = Flask(__name__)
 
-applications = ["http://localhost:8080", "http://localhost:8081", "http://localhost:8082"]
+applications = [
+    {
+        "guestUrl": "http://localhost:8080",
+        "healthCheckUrl": "server1.test"
+    },
+    {
+        "guestUrl": "http://localhost:8081",
+        "healthCheckUrl": "server2.test"
+    },
+    {
+        "guestUrl": "http://localhost:8082",
+        "healthCheckUrl": "server3.test"
+    }
+]
 
-round_robin_index = 0
+round_robin = {
+    "index": 0
+}
+
+def health_check(application):
+
+    return True
+
+    try:
+
+        response = requests.get(f'{application}/v1/ping')
+
+        if response.status_code == 200:
+
+            return True
+        
+        return False
+    
+    except:
+
+        return False
+    
 
 @app.route('/loadbalancer/apps', methods=['GET', 'POST'])
 def manage_applications():
@@ -29,16 +63,28 @@ def manage_applications():
 @app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
 def load_balancer(path):
 
-    global applications
-    global round_robin_index
+    global applications, round_robin
 
     if len(applications) == 0:
 
         return jsonify({'error': 'No applications available'}), 500
     
-    app = applications[round_robin_index]
+    app = None
+    
+    for _ in range(len(applications)):
 
-    round_robin_index = (round_robin_index + 1) % len(applications)
+        if health_check(applications[round_robin["index"]]["healthCheckUrl"]):
+            app = applications[round_robin["index"]]["guestUrl"]
+            round_robin["index"] = (round_robin["index"] + 1) % len(applications)
+            break
 
+        round_robin["index"] = (round_robin["index"] + 1) % len(applications)
+
+    
+    if app is None:
+            
+            return jsonify({'error': 'No applications available'}), 500
+
+    
     return redirect(f'{app}/{path}', 307)
     

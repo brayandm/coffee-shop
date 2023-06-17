188fbebb45a7fd1d993633270cc4040f6c901cf5
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-17 19:27:32 +0200

    update

---
+++ load_balancer.py
@@ -0,0 +1,26 @@
+from flask import Flask, Blueprint, jsonify, request
+import base64
+import os
+
+app = Flask(__name__)
+
+applications = ["server1.test", "server2.test", "server3.test"]
+
+@app.route('/loadbalancer/apps', methods=['GET', 'POST'])
+def favourite_coffee():
+
+    print(applications)
+    
+    if request.method == 'GET':
+
+        return jsonify({"data": {"applications": applications}}), 200
+    
+    elif request.method == 'POST':
+
+        if not request.json or not 'applications' in request.json:
+            return jsonify({'error': 'Bad request'}), 400
+
+        applications = request.json['applications']
+
+        return jsonify({"data": {"applications": applications}}), 200
+        
